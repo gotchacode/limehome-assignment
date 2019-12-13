7bb3a8105f43e9f2e07695a976bb2e460689d053
Vinit Kumar <mail@vinitkumar.me>
2019-12-13 15:22:19 +0530

    feat: add some more assertions in tests

--- hotels/tests/test_views.py
+++ hotels/tests/test_views.py
@@ -11,14 +11,14 @@ class TestHotelsAPI(LiveServerTestCase):
         self.api_url = f"{self.live_server_url}{reverse('hotels-search-api')}"
 
     def test_hotels_api_correct_coord(self):
-        print(self.api_url)
         final_api_url = f"{self.api_url}?at={self.valid_coord}"
         response = self.client.get(final_api_url)
+        assert 'title' in response.json()[0].keys()
+        assert 'position' in response.json()[0].keys()
         assert len(response.json()) > 0
         assert response.status_code == 200
 
     def test_hotels_api_incorrect_coord(self):
-        print(self.api_url)
         final_api_url = f"{self.api_url}?at={self.invalid_coord}"
         response = self.client.get(final_api_url)
         assert len(response.json()) == 0
